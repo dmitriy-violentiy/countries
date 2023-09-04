56d623272f63606c4f2a61d9403e575a080aa3ec
violentiy1997 <violentiy1997@mail.ru>
2023-09-04 14:41:49 +0300

    add structure and header component

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,8 +1,9 @@
+import { Header } from "./components/Header";
+
 function App() {
 	return (
 		<div className="App">
-			<p>asd</p>
-			<h1>asd</h1>
+			<Header />
 		</div>
 	);
 }
---
+++ countries/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { IoMoon, IoMoonOutline } from 'react-icons/io5'
+
+import { Container } from './Container'
+
+const HeaderEl = styled.header``;
+const Wrapper = styled.div``;
+const Title = styled.a.attrs({
+   href: '/', 
+})``;
+
+const ModeSwitcher = styled.div``;
+
+export const Header = () => {
+   return (
+      <HeaderEl>
+         <Container>
+            <Wrapper>
+               <Title>Where is the world?</Title>
+               <ModeSwitcher>
+                  <IoMoon /> Light theme
+               </ModeSwitcher>
+            </Wrapper>
+         </Container>
+      </HeaderEl>
+   )
+}
