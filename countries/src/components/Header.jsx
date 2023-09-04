d918195827d3adcfb74692fa5e73c07ff6dbc7d2
violentiy1997 <violentiy1997@mail.ru>
2023-09-04 15:07:22 +0300

    add text change when changing theme

--- countries/src/components/Header.jsx
+++ countries/src/components/Header.jsx
@@ -8,12 +8,27 @@ const HeaderEl = styled.header`
    box-shadow: var(--shadow);
    background-color: (--colors-ui-base)
 `;
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   padding: 2rem 0;
+`;
 const Title = styled.a.attrs({
    href: '/', 
-})``;
+})`
+   color: var(--colors-text);
+   font-size: var(--fs-sm);
+   text-decoration: none;
+   font-weight: var(--fw-bold);
+`;
 
-const ModeSwitcher = styled.div``;
+const ModeSwitcher = styled.div`
+   color: var(--colors-text);
+   font-size: var(--fs-sm);
+   cursor: pointer;
+   text-transform: capitalize;
+`;
 
 export const Header = () => {
    const [theme, setTheme] = useState('light')
@@ -29,7 +44,12 @@ export const Header = () => {
             <Wrapper>
                <Title>Where is the world?</Title>
                <ModeSwitcher onClick={toggleTheme}>
-                  <IoMoon /> Light theme
+                  {theme === 'light' ? (
+                     <IoMoonOutline size="14px" />
+                  ) : (
+                     <IoMoon size="14px" />
+                  )}
+                  <span style={{marginLeft: '0.75rem'}}>{theme} theme</span>
                </ModeSwitcher>
             </Wrapper>
          </Container>
