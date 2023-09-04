2818c1d188248213e3e7d5429878ae97af438a85
violentiy1997 <violentiy1997@mail.ru>
2023-09-04 14:53:41 +0300

    add functional change theme

--- countries/src/components/Header.jsx
+++ countries/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
+import { useState, useEffect } from 'react'
 import { IoMoon, IoMoonOutline } from 'react-icons/io5'
 
 import { Container } from './Container'
 
-const HeaderEl = styled.header``;
+const HeaderEl = styled.header`
+   box-shadow: var(--shadow);
+   background-color: (--colors-ui-base)
+`;
 const Wrapper = styled.div``;
 const Title = styled.a.attrs({
    href: '/', 
@@ -12,12 +16,19 @@ const Title = styled.a.attrs({
 const ModeSwitcher = styled.div``;
 
 export const Header = () => {
+   const [theme, setTheme] = useState('light')
+
+   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+
+   useEffect(() => {
+      document.body.setAttribute('data-theme', theme)
+   }, [theme])
    return (
       <HeaderEl>
          <Container>
             <Wrapper>
                <Title>Where is the world?</Title>
-               <ModeSwitcher>
+               <ModeSwitcher onClick={toggleTheme}>
                   <IoMoon /> Light theme
                </ModeSwitcher>
             </Wrapper>
